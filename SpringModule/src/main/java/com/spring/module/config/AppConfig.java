28d8f7ac963736bbfaf59131f2d7035583246682
Karol <karol.kolanczak@hotmail.com>
2019-06-11 13:24:03 +0200

    Spring module: adding configuration for Rest

---
+++ SpringModule/src/main/java/com/spring/module/config/AppConfig.java
@@ -0,0 +1,13 @@
+package com.spring.module.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan
+//@ComponentScan(basePackages ="com.spring.module")
+public class AppConfig {
+
+}
---
+++ SpringModule/src/main/java/com/spring/module/controller/SpringModuleRestController.java
@@ -0,0 +1,16 @@
+package com.spring.module.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class SpringModuleRestController {
+
+    @GetMapping("/hello")
+//    @GetMapping("/")
+    public String sayHello(){
+         return "Witka witka";
+    }
+}
