1352c922dc18ef0e28d0dc8649277dee00d479d2
Karol <karol.kolanczak@hotmail.com>
2019-06-23 20:41:05 +0200

    Spring module: refactoring code-rest controller method 'getImageByHolidayId'

--- SpringModule/src/main/java/com/spring/module/controller/HolidayRestContoller.java
+++ SpringModule/src/main/java/com/spring/module/controller/HolidayRestContoller.java
@@ -2,12 +2,22 @@ package com.spring.module.controller;
 
 import com.spring.module.model.Holiday;
 import com.spring.module.service.HolidayService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.ArrayUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.imageio.ImageIO;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,9 +30,53 @@ public class HolidayRestContoller {
     HolidayService holidayService;
 
     @GetMapping("/holidays")
-    public List<Holiday> getListOfHolidays(){
+    public List<Holiday> getListOfHolidays() throws IOException {
         List<Holiday> listOfHolidays=new ArrayList<>();
+        List<Holiday> listOfHolidays2=new ArrayList<>();
         listOfHolidays=holidayService.getHolidays();
+
+        for(Holiday value: listOfHolidays){
+//            convertFromMultipartFileToByteFormatFile(value.getImage());
+            convertByteArrayToBufferedImage(value.getImage());
+        }
+
         return listOfHolidays;
     }
+
+    // convert byte array back to BufferedImage
+    void convertByteArrayToBufferedImage(Byte[] byteImageObject) throws IOException {
+
+        byte[] bytes = ArrayUtils.toPrimitive(byteImageObject);
+
+        InputStream in = new ByteArrayInputStream(bytes);
+        BufferedImage bImageFromConvert = ImageIO.read(in);
+
+        ImageIO.write(bImageFromConvert, "jpg", new File(
+                "SpringModule/src/main/resources/1.jpg"));
+        System.out.println("Done");
+    }
+
+    byte[]  convertToByteOnly(Byte[] byteImageObject) throws IOException {
+        byte[] bytes = ArrayUtils.toPrimitive(byteImageObject);
+        return bytes;
+}
+
+@GetMapping(value = "/image/{holidayId}",produces = MediaType.IMAGE_JPEG_VALUE)
+//public @ResponseBody
+    public byte[] getImageByHolidayId(@PathVariable int holidayId) throws IOException {
+
+        List<Holiday> listOfHolidays=holidayService.getHolidays();
+
+        byte[] bytes = new byte[0];
+
+        for(Holiday value: listOfHolidays){
+
+            if(value.getId()==holidayId){
+                bytes = convertToByteOnly(value.getImage());
+            }
+        }
+        return bytes;
+    }
+
+
 }
