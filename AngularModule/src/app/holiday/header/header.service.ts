7eb11cd707e3b12b62a4596607f4c75a70423bae
Karol <karol.kolanczak@hotmail.com>
2019-09-16 08:25:41 +0200

    Angular module: adding header service

---
+++ AngularModule/src/app/holiday/header/header.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import {Subject} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeaderService {
+
+  isLoading=new Subject<boolean>();
+  constructor() { }
+
+
+}
